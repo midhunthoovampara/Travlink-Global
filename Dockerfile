f5359d7b650dcfb7a3711c6b98b25fab150f5bec
md <midhundas1422001@gmail.com>
2026-07-12 01:05:20 +0530

    third commit

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+﻿FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
+WORKDIR /src
+
+COPY Cargon.csproj ./
+RUN dotnet restore Cargon.csproj
+
+COPY . .
+RUN dotnet publish Cargon.csproj -c Release -o /app/publish /p:UseAppHost=false
+
+FROM mcr.microsoft.com/dotnet/aspnet:9.0 AS runtime
+WORKDIR /app
+
+ENV ASPNETCORE_ENVIRONMENT=Production
+EXPOSE 10000
+
+COPY --from=build /app/publish .
+ENTRYPOINT ["dotnet", "Cargon.dll"]
--- obj/Debug/net9.0/Cargon.AssemblyInfo.cs
+++ obj/Debug/net9.0/Cargon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cargon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d8de1c32c907bf37d5c68a5049783a8dadcc8bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f2149a1654dd8da35fdd3a4eb9a0ec0210cdecb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cargon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cargon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
